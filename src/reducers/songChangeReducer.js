821bbadcd15a637acd425ddfafa4d0004233828b
Student <example@example.com>
2019-09-23 14:09:05 -0700

    i thunk it works

--- src/reducers/songChangeReducer.js
+++ src/reducers/songChangeReducer.js
@@ -1,12 +1,12 @@
-import constants from './../constants';
+import constants from "./../constants";
 const { initialState, types } = constants;
 
 const songChangeReducer = (state = initialState.currentSongId, action) => {
   switch (action.type) {
-  case types.CHANGE_SONG:
-    return action.newSelectedSongId;
-  default:
-    return state;
+    case types.CHANGE_SONG:
+      return action.newSelectedSongId;
+    default:
+      return state;
   }
 };
 
